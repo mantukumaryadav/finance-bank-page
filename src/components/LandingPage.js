c44ef4f268f41ff2a5e32cce176f082273b33184
mantukumaryadav <mantukumarsiwan6@gmail.com>
2024-06-03 22:36:54 +0530

    first commit

---
+++ src/components/LandingPage.js
@@ -0,0 +1,425 @@
+import "./home.css";
+
+const home = () => {
+  return (
+    <div className="app-landing-page">
+      <img
+        className="app-landing-page-child"
+        alt=""
+        src="/assets/group-1000002356.svg"
+      />
+      <img className="app-landing-page-item" alt="" src="/assets/group-35932@2x.png" />
+      <img
+        className="app-landing-page-inner"
+        alt=""
+        src="/assets/group-1000002356.svg"
+      />
+      <img className="group-icon" alt="" src="/assets/group-1000002356.svg" />
+      <img
+        className="app-landing-page-child1"
+        alt=""
+        src="/assets/group-1000002356.svg"
+      />
+      <img className="app-landing-page-child2" alt="" src="/assets/group-35930.svg" />
+      <div className="group-parent">
+        <div className="vector-parent">
+          <img className="group-child" alt="" src="/assets/rectangle-4.svg" />
+          <img className="group-item" alt="" src="/assets/rectangle-5.svg" />
+          <img className="group-inner" alt="" src="/assets/star-7.svg" />
+          <div className="rectangle-div" />
+          <div className="group-child1" />
+          <img className="rectangle-icon" alt="" src="/assets/rectangle-3.svg" />
+          <div className="make-the-best">make the best financial decisions</div>
+          <div className="rectangle-parent">
+            <div className="group-child2" />
+            <img className="group-child3" alt="" src="/assets/group-35920@2x.png" />
+          </div>
+          <div className="uifry-premium-parent">
+            <div className="uifry-premium">Uifry Premium</div>
+            <div className="free-trial">free trial</div>
+          </div>
+        </div>
+        <b className="a">a</b>
+      </div>
+      <img className="app-landing-page-child3" alt="" src="/assets/group-35899.svg" />
+      <img className="app-landing-page-child4" alt="" src="/assets/group-35896.svg" />
+      <img className="rectangle-icon1" alt="" src="/assets/rectangle@2x.png" />
+      <div className="frame">
+        <img className="frame-child" alt="" src="/assets/group-35905.svg" />
+        <img className="frame-item" alt="" src="/assets/group-35905.svg" />
+        <div className="frame-inner">
+          <div className="ready-to-get-started-parent">
+            <b className="ready-to-get">ready to get started?</b>
+            <div className="risus-habitant-leo">
+              Risus habitant leo egestas mauris diam eget morbi tempus
+              vulputate.
+            </div>
+            <div className="rectangle-group">
+              <div className="group-child4" />
+              <div className="vector-group">
+                <img className="vector-icon" alt="" src="/assets/vector.svg" />
+                <div className="download-app-wrapper">
+                  <div className="download-app">download app</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <img className="frame-child1" alt="" src="/assets/group-35932@2x.png" />
+        <img className="iphone-13-pro" alt="" src="/assets/iphone-13-pro@2x.png" />
+        <img className="star-icon" alt="" src="/assets/star-71.svg" />
+        <img className="frame-child2" alt="" src="/assets/star-6.svg" />
+      </div>
+      <img className="frame-icon" alt="" src="/assets/frame-1@2x.png" />
+      <b className="make-the-best1">make the best financial decisions</b>
+      <div className="cum-et-convallis">
+        Cum et convallis risus placerat aliquam, nunc. Scelerisque aliquet
+        faucibus tincidunt eu adipiscing sociis arcu lorem porttitor.
+      </div>
+      <div className="group-container">
+        <div className="rectangle-container">
+          <div className="group-child5" />
+          <div className="get-started-parent">
+            <div className="get-started">get started</div>
+            <img className="group-child6" alt="" src="/assets/vector-1.svg" />
+          </div>
+        </div>
+        <div className="group-div">
+          <img className="group-child7" alt="" src="/assets/group-3.svg" />
+          <div className="watch-video">watch video</div>
+        </div>
+      </div>
+      <img className="app-landing-page-child5" alt="" src="/assets/star-3.svg" />
+      <img className="app-landing-page-child6" alt="" src="/assets/star-3.svg" />
+      <img
+        className="iphone-13-pro-front-icon"
+        alt=""
+        src="/assets/iphone13profront@2x.png"
+      />
+      <img
+        className="iphone-13-pro-front-icon1"
+        alt=""
+        src="/assets/iphone13profront@2x.png"
+      />
+      <div className="group-parent1">
+        <div className="auto-layout-horizontal-parent">
+          <div className="auto-layout-horizontal">
+            <b className="home">home</b>
+            <div className="about-us">about us</div>
+            <div className="about-us">pricing</div>
+            <div className="about-us">features</div>
+          </div>
+          <div className="group">
+            <div className="tm">TM</div>
+            <div className="group1">
+              <img className="vector-icon1" alt="" src="/assets/vector1.svg" />
+              <img className="frame-icon1" alt="" src="/assets/frame.svg" />
+            </div>
+          </div>
+        </div>
+        <div className="rectangle-parent1">
+          <div className="group-child5" />
+          <div className="download-wrapper">
+            <div className="download">download</div>
+          </div>
+        </div>
+      </div>
+      <div className="group-parent2">
+        <div className="what-our-users-say-about-us-parent">
+          <b className="what-our-users">what our users say about us?</b>
+          <div className="testimonial">testimonial</div>
+        </div>
+        <div className="group-parent3">
+          <div className="arcu-at-dictum-sapien-mollis-parent">
+            <div className="arcu-at-dictum">
+              “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+              ultricies. In ultrices malesuada elit mauris etiam odio. Duis
+              tristique lacus, et blandit viverra nisl velit. Sed mattis
+              rhoncus, diam suspendisse sit nunc, gravida eu. Lectus eget eget
+              ac dolor neque lorem sapien, suspendisse aliquam.”
+            </div>
+            <div className="the-best-financial">
+              the best financial accounting app ever!
+            </div>
+            <div className="group-parent4">
+              <img className="group-child9" alt="" src="/assets/group-28@2x.png" />
+              <div className="group-parent5">
+                <img className="group-child10" alt="" src="/assets/group-27@2x.png" />
+                <img className="group-child11" alt="" src="/assets/group-29@2x.png" />
+                <img className="group-child12" alt="" src="/assets/group-30@2x.png" />
+                <img className="group-child13" alt="" src="/assets/group-31@2x.png" />
+              </div>
+            </div>
+            <div className="nick-jonas">nick jonas</div>
+          </div>
+          <img className="group-child14" alt="" src="/assets/group-1000002332.svg" />
+        </div>
+      </div>
+      <div className="star-parent">
+        <img className="group-child15" alt="" src="/assets/star-5.svg" />
+        <img className="group-child16" alt="" src="/assets/star-5.svg" />
+        <img className="group-child17" alt="" src="/assets/star-5.svg" />
+        <img className="group-child18" alt="" src="/assets/star-5.svg" />
+        <img className="group-child19" alt="" src="/assets/star-2.svg" />
+        <img className="group-child20" alt="" src="/assets/star-2.svg" />
+        <img className="group-child21" alt="" src="/assets/star-2.svg" />
+        <img className="group-child22" alt="" src="/assets/star-2.svg" />
+        <div className="group-parent6">
+          <div className="group-parent7">
+            <div className="uifry-premium-group">
+              <b className="uifry-premium1">uifry premium</b>
+              <div className="features1">features</div>
+            </div>
+            <div className="group-parent8">
+              <div className="budgeting-intervals-parent">
+                <div className="budgeting-intervals">budgeting intervals</div>
+                <img className="star-05-icon" alt="" src="/assets/star05.svg" />
+              </div>
+              <div className="cum-et-convallis1">
+                Cum et convallis risus placerat aliquam, nunc. Scelerisque
+                aliquet faucibus tincidunt eu adipiscing sociis arcu lorem
+                porttitor.
+              </div>
+            </div>
+            <div className="group-parent9">
+              <div className="budgeting-intervals-parent">
+                <div className="budgeting-intervals">budgeting intervals</div>
+                <img className="star-05-icon" alt="" src="/assets/cube02.svg" />
+              </div>
+              <div className="cum-et-convallis1">
+                Cum et convallis risus placerat aliquam, nunc. Scelerisque
+                aliquet faucibus tincidunt eu adipiscing sociis arcu lorem
+                porttitor.
+              </div>
+            </div>
+            <div className="group-parent10">
+              <div className="budgeting-intervals-parent">
+                <div className="budgeting-intervals">budgeting intervals</div>
+                <img className="star-05-icon" alt="" src="/assets/cube04.svg" />
+              </div>
+              <div className="cum-et-convallis1">
+                Cum et convallis risus placerat aliquam, nunc. Scelerisque
+                aliquet faucibus tincidunt eu adipiscing sociis arcu lorem
+                porttitor.
+              </div>
+            </div>
+          </div>
+          <img className="group-child23" alt="" src="/assets/group-35933@2x.png" />
+        </div>
+        <div className="group-parent11">
+          <div className="group-parent12">
+            <div className="why-choose-uifry-parent">
+              <b className="why-choose-uifry">why choose Uifry?</b>
+              <div className="advatnages">advatnages</div>
+            </div>
+            <div className="arcu-at-dictum-sapien-mollis-group">
+              <div className="arcu-at-dictum1">
+                Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+                ultricies. In ultrices malesuada elit mauris etiam odio. Duis
+                tristique lacus, et blandit viverra nisl velit. Sed mattis
+                rhoncus, diam suspendisse sit nunc, gravida eu. Lectus eget eget
+                ac dolor neque lorem sapien, suspendisse aliquam.
+              </div>
+              <div className="clever-notifications-parent">
+                <div className="clever-notifications">clever notifications</div>
+                <img className="group-child24" alt="" src="/assets/group-358991.svg" />
+              </div>
+            </div>
+          </div>
+          <img className="group-child25" alt="" src="/assets/group-35935@2x.png" />
+        </div>
+        <div className="on-hold">
+          <div className="on-hold-child" />
+          <img className="on-hold-item" alt="" src="/assets/rectangle-13@2x.png" />
+          <div className="on-hold1">On Hold</div>
+          <div className="parent">
+            <b className="b">$560.00</b>
+            <div className="from-adom">From Adom</div>
+          </div>
+        </div>
+        <div className="group-parent13">
+          <div className="group-parent14">
+            <div className="group-wrapper">
+              <div className="arcu-at-dictum-sapien-mollis-container">
+                <div className="arcu-at-dictum2">
+                  Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+                  ultricies. In ultrices malesuada elit mauris etiam odio. Duis
+                  tristique lacus, et blandit viverra nisl velit. Sed mattis
+                  rhoncus, diam suspendisse sit nunc, gravida eu. Lectus eget
+                  eget ac dolor neque lorem sapien, suspendisse aliquam.
+                </div>
+                <div className="fully-customizable-parent">
+                  <div className="fully-customizable">fully customizable</div>
+                  <img
+                    className="group-child26"
+                    alt=""
+                    src="/assets/group-358992.svg"
+                  />
+                </div>
+              </div>
+            </div>
+            <img className="group-child27" alt="" src="/assets/group-35937@2x.png" />
+          </div>
+          <div className="group2">
+            <div className="frame1">
+              <div className="rectangle" />
+            </div>
+            <div className="div">0000 8888 2222 3333</div>
+            <div className="group3">
+              <div className="div1">07/24</div>
+              <div className="valid-thru">VALID THRU</div>
+            </div>
+            <img
+              className="credit-cardvisa-typewhite"
+              alt=""
+              src="/assets/credit-cardvisa-typewhite.svg"
+            />
+            <div className="group4">
+              <div className="tm1">TM</div>
+              <img className="group-icon1" alt="" src="/assets/group@2x.png" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="group-parent15">
+        <div className="frequently-asked-questions-parent">
+          <b className="frequently-asked-questions">
+            Frequently asked questions
+          </b>
+          <div className="faq">faq</div>
+        </div>
+        <div className="group-parent16">
+          <div className="rectangle-parent2">
+            <div className="group-child28" />
+            <div className="arcu-at-dictum-sapien-mollis-parent1">
+              <div className="arcu-at-dictum3">
+                “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+                ultricies. In ultrices malesuada elit mauris.
+              </div>
+              <div className="the-best-financial1">
+                the best financial accounting app ever!
+              </div>
+            </div>
+          </div>
+          <div className="rectangle-parent3">
+            <div className="group-child29" />
+            <div className="arcu-at-dictum-sapien-mollis-parent1">
+              <div className="arcu-at-dictum4">
+                “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+                ultricies. In ultrices malesuada elit mauris.
+              </div>
+              <div className="the-best-financial1">
+                the best financial accounting app ever!
+              </div>
+            </div>
+          </div>
+          <div className="rectangle-parent4">
+            <div className="group-child28" />
+            <div className="arcu-at-dictum-sapien-mollis-parent1">
+              <div className="arcu-at-dictum3">
+                “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+                ultricies. In ultrices malesuada elit mauris.
+              </div>
+              <div className="the-best-financial1">
+                the best financial accounting app ever!
+              </div>
+            </div>
+          </div>
+          <div className="rectangle-parent5">
+            <div className="group-child29" />
+            <div className="arcu-at-dictum-sapien-mollis-parent1">
+              <div className="arcu-at-dictum4">
+                “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+                ultricies. In ultrices malesuada elit mauris.
+              </div>
+              <div className="the-best-financial1">
+                the best financial accounting app ever!
+              </div>
+            </div>
+          </div>
+          <div className="rectangle-parent6">
+            <div className="group-child28" />
+            <div className="arcu-at-dictum-sapien-mollis-parent1">
+              <div className="arcu-at-dictum3">
+                “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+                ultricies. In ultrices malesuada elit mauris.
+              </div>
+              <div className="the-best-financial1">
+                the best financial accounting app ever!
+              </div>
+            </div>
+          </div>
+          <div className="rectangle-parent7">
+            <div className="group-child29" />
+            <div className="arcu-at-dictum-sapien-mollis-parent1">
+              <div className="arcu-at-dictum4">
+                “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+                ultricies. In ultrices malesuada elit mauris.
+              </div>
+              <div className="the-best-financial1">
+                the best financial accounting app ever!
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="frame-parent">
+        <div className="frame2">
+          <div className="copyright-2022-uifrycom">
+            Copyright 2022 uifry.com all rights reserved
+          </div>
+        </div>
+        <div className="group5">
+          <div className="take-tour">take tour</div>
+          <div className="live-chat">live chat</div>
+          <div className="reveiws">reveiws</div>
+          <div className="product">product</div>
+        </div>
+        <div className="group6">
+          <div className="terms-of-use">terms of use</div>
+          <div className="privacy-policy">privacy policy</div>
+          <div className="cookie-policy">cookie policy</div>
+          <div className="legal">legal</div>
+        </div>
+        <div className="group7">
+          <div className="home1">home</div>
+          <div className="about-us1">about us</div>
+          <div className="bookings">bookings</div>
+          <div className="blog">blog</div>
+          <div className="links">links</div>
+        </div>
+        <div className="group8">
+          <div className="group9">
+            <div className="tm2">TM</div>
+            <div className="group1">
+              <img className="frame-icon1" alt="" src="/assets/frame.svg" />
+              <img className="vector-icon1" alt="" src="/assets/vector1.svg" />
+            </div>
+          </div>
+          <div className="group11">
+            <div className="helpfrybixcom">help@frybix.com</div>
+            <img className="frame-icon3" alt="" src="/assets/frame1.svg" />
+          </div>
+          <div className="group12">
+            <div className="div2">+1 234 456 678 89</div>
+            <img className="frame-icon4" alt="" src="/assets/frame2.svg" />
+          </div>
+        </div>
+        <div className="newsletter-parent">
+          <div className="newsletter">Newsletter</div>
+          <div className="stay-up-to">Stay up to date</div>
+          <div className="rectangle-parent8">
+            <div className="rectangle1" />
+            <div className="your-email">{`Your email `}</div>
+            <div className="group13">
+              <div className="rectangle2" />
+              <div className="subscribe">Subscribe</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default home;
